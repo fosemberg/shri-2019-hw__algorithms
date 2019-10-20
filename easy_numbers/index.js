5c8949b39484809bf9814b915615c7e03496cb4b
fosemberg <fosemberg@gmail.com>
2019-10-20 15:17:19 +0300

    easy numbers with memo

--- easy_numbers/index.js
+++ easy_numbers/index.js
@@ -1,18 +1,58 @@
 const log = console.log;
 
+const memo = fn => {
+  const cache = {};
+  return (...args) => {
+    const argsString = args.toString();
+    if (argsString in cache) {
+      return cache[argsString];
+    } else {
+      let result = fn(...args);
+      cache[argsString] = result;
+      return result;
+    }
+  }
+};
+
+const withMeasureSpeed = fn => {
+  return (...args) => {
+    let start = new Date().getTime();
+    const result = fn(...args);
+    let end = new Date().getTime();
+    const tookTime = end - start;
+    log(`Took time: ${tookTime}`);
+    return result;
+  }
+};
+
+const isNoRestAfterDivision = (num1, num2) => num1 % num2 === 0;
+const memoIsNoRestAfterDivision = memo(isNoRestAfterDivision);
+
 const isEasyNumber = num => {
   if (num <= 3) {
     return true
   } else {
     for (let i = 2; i < num; i++) {
-      if (num % i === 0) return false;
+      if (memoIsNoRestAfterDivision(num, i)) return false;
     }
   }
   return true;
 };
 
-for (let i = 0; i <= 100; i++) {
-  if (isEasyNumber(i)) {
-    log(i)
+const memoIsEasyNumber = memo(isEasyNumber);
+
+const getEasyNumbers = (numbers) => {
+  const arr = [];
+  for (let i = 0; i <= numbers; i++) {
+    if (memoIsEasyNumber(i)) {
+      arr.push(i);
+    }
   }
-}
+  return arr;
+};
+
+const getEasyNumbersWithMeasureSpeed = withMeasureSpeed(getEasyNumbers);
+
+log(getEasyNumbersWithMeasureSpeed(500));
+log(getEasyNumbersWithMeasureSpeed(500));
+
