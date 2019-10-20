588f1348dd4b1d2f11a2027fca3938ab7426f97d
fosemberg <fosemberg@gmail.com>
2019-10-20 17:58:25 +0300

    work

---
+++ max_sub_sum/index.js
@@ -0,0 +1,24 @@
+const maxSubSum = arr => {
+  let currentSum = 0;
+  let maxSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    currentSum = 0;
+    for (let j = i; j < arr.length; j++) {
+      const el = arr[j];
+      currentSum += el;
+      if (currentSum < 0) {
+        currentSum = 0;
+      }
+      if (currentSum > maxSum) {
+        maxSum = currentSum;
+      }
+    }
+  }
+
+  return maxSum;
+};
+
+const arr = [-4, 2, 0, 5, 0, 3, -9, 10, 11, 4];
+
+console.log(maxSubSum(arr));
